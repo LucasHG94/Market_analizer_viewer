3ff3cfdb83b37dba19da4bf45987df75a2e2712f
sturm <sturm@localhost.localdomain>
2017-06-07 12:35:39 +0200

    Tabs para las diferentes gráficas

--- src/app/+company/company.component.ts
+++ src/app/+company/company.component.ts
@@ -1,6 +1,5 @@
-import {AfterContentInit, Component, ElementRef, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit} from '@angular/core';
 import {ProxyService} from '../proxy.service';
-import {Company, StateBonus} from '../model/index';
 import {Subscription} from 'rxjs/Subscription';
 import {ActivatedRoute} from '@angular/router';
 
@@ -8,243 +7,32 @@ import {ActivatedRoute} from '@angular/router';
 declare var Highcharts: any;
 declare var jQuery: any;
 
-
-export interface ChartConfig {
-  title: string;
-  yAxes?: ChartYAxis[];
-  series?: ChartSeries[];
-}
-
-export interface ChartYAxis {
-  labels: {
-    format: string;
-  };
-  title: {
-    text: string
-  };
-  opposite: boolean;
-  showEmpty: boolean;
-}
-
-export interface ChartSeries {
-  name: string;
-  type: string;
-  yAxis: number;
-  data: any[];
-  tooltip?: {
-    valueSuffix: string;
-  };
-  realm?: string;
-}
-
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
   styleUrls: ['./company.component.css']
 })
-export class CompanyComponent implements OnInit, AfterContentInit {
+export class CompanyComponent implements OnInit {
   private routeSub: Subscription;
   private companyId: number;
-  private optionSelected: number = 1;
-  private from: Date;
-  private to: Date;
-  private rangeSelected: boolean = false;
-  private title: string;
-  private chart: any;
-  private series: any[];
-  private data: any[];
-  private seriesName: string[] = [
-    'price',
-    'difference',
-    'percentageDifference',
-    'capitalization',
-    'BPA',
-    'PER',
-    'PVC',
-    'PCF',
-    'dividendYield',
-    'interestPerShare',
-    'lastValue',
-    'max',
-    'min',
-    'volume'
-  ];
-
-  company: Company;
-  stateBonus: StateBonus[];
+  private companyName: string;
   constructor (private el: ElementRef, private proxyService: ProxyService, private route: ActivatedRoute) {
   }
 
   ngOnInit() {
-    this.to = new Date();
-    this.from = new Date();
-    this.from.setMonth(this.to.getMonth() - 1);
     this.routeSub = this.route.params.subscribe(params => {
       this.companyId = +params['companyId'];
-      this.updateView();
+      this.companyName = params['companyName'];
+      console.log(this.companyName);
     });
   }
 
-  ngAfterContentInit() {
-    let that = this;
-    let el = this.el.nativeElement;
-    jQuery(() => {
-      jQuery(el).find('#reservation').daterangepicker(
-        {
-          locale: {
-            format: 'DD-MM-YYYY'
-          },
-          startDate: this.from.getDate() + '-' + (this.from.getMonth() + 1) + '-' + this.from.getFullYear(),
-          endDate: this.to.getDate() + '-' + (this.to.getMonth() + 1) + '-' + this.to.getFullYear()
-        },
-        function (start: Date, end: Date, label: any) {
-          that.from = new Date(start);
-          that.to = new Date(end);
-          that.updateView();
-        });
-    });
-  }
-
-  private updateView() {
-    this.proxyService.getCompanyData(this.companyId, this.from.getTime(), this.to.getTime()).then(company => {
-      this.company = Company.fromRaw(company);
-      this.title = this.company.name;
-      let date = new Date();
-      Highcharts.setOptions({
-        global: {
-          getTimezoneOffset: date.getTimezoneOffset.bind(date)
-        },
-        credits: {
-          enabled: false
-        },
-        chart: {
-          resetZoomButton: {
-            position: {
-              // y: -40
-            }
-          }
-        }
+  reflowCharts() {
+    setTimeout(() => {
+      jQuery('.chart').each((index: any, value: any) => {
+        let chartHandler = jQuery(value).highcharts();
+        if (chartHandler) {chartHandler.reflow(); }
       });
-      this.proxyService.getStateBonus(this.from.getTime(), this.to.getTime()).then(stateBonus => {
-        this.stateBonus = stateBonus.map(StateBonus.fromRaw);
-        this.stateBonus.sort(function(a, b){return a.date.getTime() - b.date.getTime(); });
-        this.loadData();
-        this.initGraph();
-      });
-    });
-  }
-
-  loadData() {
-    this.data = [];
-    let stateBonusData = [];
-    for (let name of this.seriesName){
-      this.data.push([]);
-      for (let dailyData of this.company.dailyData){
-        this.data[this.data.length - 1].push([dailyData.date.getTime(), dailyData[name]]);
-      }
-    }
-    for (let stateB of this.stateBonus){
-      stateBonusData.push([stateB.date.getTime(), stateB.stateBonus]);
-    }
-    this.series = [];
-    for (let i = 0; i < this.data.length; i++) {
-      if (this.seriesName[i] !== 'BPA' && this.seriesName[i] !== 'PER') {
-        this.series.push({
-          name: this.seriesName[i],
-          data: this.data[i],
-          visible: false
-        });
-      } else {
-        this.series.push({
-          name: this.seriesName[i],
-          data: this.data[i],
-          visible: true
-        });
-      }
-    }
-    this.series.push({
-      name: 'Sate Bonus',
-      data: stateBonusData,
-      visible: true
-    });
-  }
-
-  initGraph() {
-    let el = this.el.nativeElement;
-    let $chartEl = jQuery(el).find('.chart');
-
-    this.chart = $chartEl.highcharts({
-      chart: {
-        type: 'line',
-        zoomType: 'x'
-      },
-      title: {
-        text: 'Datos generales',
-        // x: -20
-      },
-      xAxis: {
-        type: 'datetime',
-        dateTimeLabelFormats: {
-          day: '%e %b',
-        },
-        title: {
-          text: 'Fecha'
-        }
-      },
-      yAxis: {
-        title: {
-          text: 'Datos de la empresa'
-        }
-      },
-      tooltip: {
-        shared: true,
-        crosshairs: true,
-        dateTimeLabelFormats: {
-          day: '%e %b',
-        }
-      },
-      legend: {
-        enable: false,
-      },
-      series: this.series
-    });
-
-    this.chart = $chartEl.highcharts();
-  }
-
-  formatRange(from: Date, to: Date) {
-    let range: string;
-    range = from.getDate() + '-' + (from.getMonth() + 1) + '-' + from.getFullYear();
-    range += ', ';
-    range += to.getDate() + '-' + (to.getMonth() + 1) + '-' + to.getFullYear();
-    return range;
-  }
-
-  private radioChange(option: number) {
-    switch (option) {
-      case 1:
-        this.optionSelected = 1;
-        this.to = new Date();
-        this.from = new Date();
-        this.from.setMonth(this.to.getMonth() - 1);
-        this.rangeSelected = false;
-        this.updateView();
-        break;
-      case 2:
-        this.optionSelected = 2;
-        this.to = new Date();
-        this.from = new Date();
-        this.from.setMonth(this.to.getMonth() - 3);
-        this.rangeSelected = false;
-        this.updateView();
-        break;
-      case 3:
-        this.optionSelected = 3;
-        this.rangeSelected = true;
-        this.updateView();
-        break;
-      default:
-        this.updateView();
-    }
+    }, 300);
   }
 }
