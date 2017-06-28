0753ea48b0d94d40ce06fc680b3157e1fc3d00e7
sturm <sturm@localhost.localdomain>
2017-06-28 18:15:53 +0200

    Clave de acceso requerida

--- src/app/main-sidebar/main-sidebar.component.ts
+++ src/app/main-sidebar/main-sidebar.component.ts
@@ -1,8 +1,10 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, Component, ElementRef, } from '@angular/core';
 import 'rxjs/add/operator/map';
 import {ProxyService} from '../proxy.service';
 import 'rxjs/add/operator/toPromise';
 import {Company} from '../model/index';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'app-main-sidebar',
@@ -13,9 +15,15 @@ export class MainSidebarComponent {
 
   IBEX35: Company[];
   BME: Company[];
-  constructor (private proxyService: ProxyService) {
+  password: string;
+  logged: boolean = false;
+  constructor (private el: ElementRef, private proxyService: ProxyService) {
+    this.getCompanies();
+  }
+  getCompanies() {
     this.IBEX35 = [];
     this.proxyService.getCompanies().then(companies => {
+      this.mangeModal();
       this.BME = companies.map(Company.fromRaw);
       for (let company of this.BME){
         if (company.marketType === 'IBEX35') {
@@ -23,5 +31,22 @@ export class MainSidebarComponent {
         }
       }
     });
+  }
+
+  mangeModal() {
+    let that = this;
+    jQuery(this.el.nativeElement).children('#managerModal').modal('show');
+    jQuery(this.el.nativeElement).children('#managerModal').on('hidden.bs.modal', function () {
+      if (!that.logged) {
+        jQuery(that.el.nativeElement).children('#managerModal').modal('show');
+      }
+    });
+  }
+
+  access() {
+    if (this.password === '123') {
+      this.logged = true;
+      jQuery(this.el.nativeElement).children('#managerModal').modal('hide');
+    }
   }
 }
